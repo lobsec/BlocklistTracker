649dec403642a8181288172aabe9ec3f44334f00
Lobst3r <mauro@lobsec.com>
2023-07-10 17:06:48 +0200

    Aggiunti i feed di Censys

--- generate.py
+++ generate.py
@@ -1,21 +1,28 @@
 import requests
 
 urls = {
-    "ABUSE.CH BOTNET C2 IOC": "https://feodotracker.abuse.ch/downloads/ipblocklist.txt",
-    "ABUSE.CH BOTNET C2": "https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt",
-    "BLOCKLIST.DE": "https://lists.blocklist.de/lists/all.txt",
-    "SGBOX BAD REPUTATION IP": "https://download.sgbox.it/dnld/feeds/sgbox_badreputation_ip.txt",
-    "SGBOX BOTNET": "https://download.sgbox.it/dnld/feeds/sgbox_botnet.txt",
-    "TOR EXIT POINT": "https://check.torproject.org/torbulkexitlist"
+    "ABUSE.CH BOTNET C2 IOC": [0, "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"],
+    "ABUSE.CH BOTNET C2": [0, "https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt"],
+    "BLOCKLIST.DE": [0, "https://lists.blocklist.de/lists/all.txt"],
+    "SGBOX BAD REPUTATION IP": [0, "https://download.sgbox.it/dnld/feeds/sgbox_badreputation_ip.txt"],
+    "SGBOX BOTNET":  [0, "https://download.sgbox.it/dnld/feeds/sgbox_botnet.txt"],
+    "TOR EXIT POINT": [0, "https://check.torproject.org/torbulkexitlist"],
+    "IPC2s-30day": [1, "https://raw.githubusercontent.com/drb-ra/C2IntelFeeds/master/feeds/IPC2s-30day.csv"],
+    "IPC2s": [1, "https://raw.githubusercontent.com/drb-ra/C2IntelFeeds/master/feeds/IPC2s.csv"]
+
 }
+
 ips = []
 
 for site, url in urls.items():
-    req = requests.get(url)
+    req = requests.get(url[1])
     if req.status_code == 200:
         for ip in req.text.split("\n"):
             if not ip.replace("\r", "").startswith("#") and (ip.replace("\r", "") not in ips):
-                ips.append(ip.replace("\r", ""))
+                if url[0] == 0:
+                    ips.append(ip.replace("\r", ""))
+                else:
+                    ips.append(ip.replace("\r", "").split(",")[0])
 
 with open("out.txt", "w") as f_out:
     for ip in ips:
